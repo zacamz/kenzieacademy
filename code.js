d697ba05aa8534708a5bb819a4bedd92236703fc
unknown <vepped@gmail.com>
2022-02-26 21:49:33 -0500

    corrected one error with in zach J portfolio boardsports in the nav bar, added one # to fix the scroll. also changed all of the week titles from class to ID so the nav menu would start working

---
+++ code.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+  if (details.open) 
+      
+  
+});
+
+const createTitle = function () {
+    document.write("<h1>Page Title</h1>");
+  }
+
+  createTitle()
